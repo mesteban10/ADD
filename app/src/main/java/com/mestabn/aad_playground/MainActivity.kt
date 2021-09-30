e8e0d661b865aec5c68b585a3ccd14cab304f798
Maria <maria.estmar.1@educa.jcyl.es>
2021-09-30 20:38:07 +0200

    Strat project.

---
+++ app/src/main/java/com/mestabn/aad_playground/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.mestabn.aad_playground
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mesteban.aad_playground.ut_01.FilePlayGround
+
+class MainActivity : AppCompatActivity() {
+
+    //Listado de string
+
+    val colors : MutableList<String> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initColors()
+        val filePlayGround = FilePlayGround(this)
+
+        filePlayGround.saveToFile(colors)
+
+        val colors = filePlayGround.readFromFile()
+        colors.forEach {
+            Log.d("@dev", it)
+        }
+
+    }
+
+    private fun initColors(){
+        colors.add("Red")
+        colors.add("Blue")
+        colors.add("Yellow")
+        colors.add("Pink")
+    }
+}
---
+++ app/src/main/java/com/mestabn/aad_playground/ut_01/FilePlayGround.kt
@@ -0,0 +1,136 @@
+package com.mesteban.aad_playground.ut_01
+
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
+/**
+ * constructor por defecto->()
+ * una clase se compone por: estados comportamiento
+ */
+
+class FilePlayGround(private val activity: AppCompatActivity) {
+
+    /**
+     * funcion que se ejecuta al instanciar la clase(crear un objeto)
+     *
+     */
+    init {
+        //createFile()
+        // writeFile()
+        //readFile()
+        //appendText()
+        //pendTextWithNewLine()
+        //readLineByLine()
+        //deleteFile()
+
+    }
+
+    /*
+    fun createFile(){
+        val file= File(activity.filesDir,"add.txt") //Ruta interna donde se instala la aplicacion y nombre del fichero
+        Log.d("@dev",activity.filesDir.absolutePath) //Crea mensajes que se muestran en Logcat
+    }
+    */
+
+    fun writeFile() {
+        val file = File(activity.filesDir, "add.txt") //Ruta interna donde se instala la aplicacion y nombre del fichero
+        file.writeText("hola acceso a datos") //Crea mensajes que se muestran en Logcat
+    }
+
+    fun readFile() {
+        val file = File(activity.filesDir, "add.txt") //Ruta interna donde se instala la aplicacion y nombre del fichero
+        val line = file.readText() //Leo el texto y guardo en variable
+        Log.d("@dev", line)
+    }
+
+
+    /*
+    Metodo que añade una linea al fichero
+     */
+
+    fun appendText(){
+        val file= File(activity.filesDir,"add.txt") //Ruta interna donde se instala la aplicacion y nombre del fichero
+        file.appendText("Hola2") //metodo que añade linea
+        file.appendText("Hola3")
+        file.appendText("Hola4")
+        val line = file.readText()
+        Log.d("@dev",line)
+    }
+
+    /*
+    añadir texto en nueva linea
+     */
+
+    fun appendTextWithNewLine() {
+        val file = File(activity.filesDir, "add.txt") //Ruta interna donde se instala la aplicacion y nombre del fichero
+        file.appendText("\n") //metodo que añade linea
+        file.appendText("Adios1")
+        file.appendText("\n")
+        file.appendText("Adios2")
+        val line = file.readText()
+        Log.d("@dev", line)
+    }
+
+
+    //Leer linea por linea
+    fun readLineByLine(){
+        val file = File(activity.filesDir, "add.txt")
+        file.writeText("Linea 1")
+        file.appendText("\n")
+        file.appendText("Linea 2")
+        file.appendText("\n")
+        file.appendText("Linea 3")
+
+        val lines : List<String> = file.readLines()
+        //froma de recorrer un listado de objetos -->forEach
+        lines.forEach{
+            Log.d("@dev", it)
+        }
+
+
+
+    }
+
+    fun deleteFile(){
+        val file = File(activity.filesDir, "add.txt")
+        if(file.exists()){
+            file.delete()
+        }
+    }
+
+        /**
+         * Guarda un array de strings en un fichero
+         */
+    fun saveToFile(colors:MutableList<String>){
+        val file = File(activity.filesDir, "colors.txt")
+            if(file.exists()){
+                file.writeText("") //Escribo fichero desde cero, vacio
+            }
+
+        //voy metiendo los colores en el fichero
+        colors.forEach {
+            file.appendText("$it\n") //color
+        }
+
+    }
+
+    /**
+     * Funcion que lee un fichero y guarda los colores
+     */
+
+    fun readFromFile(): MutableList<String> {
+        val file = File(activity.filesDir, "colors.txt")
+
+        return if (file.exists()){
+            file.readLines().toMutableList()
+        }else
+            mutableListOf()
+        }
+
+    }
+
+
+
+
+
