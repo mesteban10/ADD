b03f1a87046942db0a2443ef51a624c5278f51d1
mesteban10 <mariaesteban1997@gmail.com>
2021-10-08 17:23:11 +0200

    Creamos nueva actividad, SharedPreferencesActivity

---
+++ app/src/main/java/com/mestabn/aad_playground/ut02/SharedPreferencesActivity.kt
@@ -0,0 +1,20 @@
+package com.mestabn.aad_playground.ut02
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mestabn.aad_playground.R
+
+class SharedPreferencesActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preferences)
+
+
+    }
+
+
+
+}
