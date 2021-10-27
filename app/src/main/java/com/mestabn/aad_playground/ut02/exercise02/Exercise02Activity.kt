e8e6ae4b9cb87659015e9c4619d926834acae3d3
mesteban10 <mariaesteban1997@gmail.com>
2021-10-27 16:55:10 +0200

    Creamos nueva actividad

---
+++ app/src/main/java/com/mestabn/aad_playground/ut02/exercise02/Exercise02Activity.kt
@@ -0,0 +1,11 @@
+package com.mestabn.aad_playground.ut02.exercise02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exercise02Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise02)
+    }
+}
