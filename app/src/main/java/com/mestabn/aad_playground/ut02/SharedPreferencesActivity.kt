40b4f6642702b182eaead4d271d49746731115f9
mesteban10 <mariaesteban1997@gmail.com>
2021-10-27 11:28:55 +0200

    Ejemplo de como guardar de manera encriptada y no encriptada en sharedPreferences.

--- app/src/main/java/com/mestabn/aad_playground/ut02/SharedPreferencesActivity.kt
+++ app/src/main/java/com/mestabn/aad_playground/ut02/SharedPreferencesActivity.kt
@@ -7,14 +7,28 @@ import com.mestabn.aad_playground.R
 
 class SharedPreferencesActivity : AppCompatActivity() {
 
+    private val TAG = SharedPreferencesActivity::class.java.canonicalName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preferences)
-
-
+        saveAsyncSharedpref()
+        saveAsyncEncryptSharedpref()
     }
 
+    private fun saveAsyncSharedpref() {
+        val localDataSource = LocalDataSource(this)
+        localDataSource.saveAsync("1", "Hola1")
+        val data = localDataSource.read("1")
+        Log.d(TAG, data!!)
+    }
+
+    private fun saveAsyncEncryptSharedpref() {
+        val localDataSource = LocalDataSource(this)
+        localDataSource.saveAsyncEncrypt("1", "Hola1")
+        val data = localDataSource.readEncrypt("1")
+        Log.d(TAG, data!!)
+    }
 
 
 }
