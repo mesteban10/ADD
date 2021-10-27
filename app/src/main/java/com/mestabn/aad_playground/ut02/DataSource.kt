c058add028415b803a157c2506adc13c00ed3d8b
mesteban10 <mariaesteban1997@gmail.com>
2021-10-27 11:31:40 +0200

    Creamos interfaz geneérica que define una fuente de datos sin especificar el tipo. Añadimos dos tipos de fuentes de datos que heredan de la interfaz (memDatasource, sharedPereferences)

---
+++ app/src/main/java/com/mestabn/aad_playground/ut02/DataSource.kt
@@ -0,0 +1,82 @@
+package com.mestabn.aad_playground.ut02
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.mestabn.aad_playground.R
+
+
+/**
+ * Interfaz genérica que define una fuente de datos sin especificar el tipo.
+ */
+interface DataSource<T : Model> {
+    fun save(model: List<T>)
+    fun fetch(id: Int): T?
+    fun fetchAll(): List<T>?
+}
+
+/**
+ * Clase que permite almacenar objetos en memoria.
+ */
+class MemDataSource<T : Model> : DataSource<T> {
+
+    private val memDataStorage: MutableList<T> = mutableListOf()
+
+    override fun save(model: List<T>) {
+        model.forEach {
+            memDataStorage.add(it)
+        }
+    }
+
+    override fun fetch(id: Int): T? {
+        memDataStorage.forEach {
+            if (it.getId() == id) {
+                return it
+            }
+        }
+        return null
+    }
+
+    override fun fetchAll(): List<T>? = memDataStorage.toList()
+}
+
+/**
+ * Clase que permite almacenar objetos en un fichero SharedPreferences
+ */
+class SharPrefDataSource<T : Model>(private val context: AppCompatActivity) : DataSource<T> {
+
+    private val gson = Gson()
+    private val type = object : TypeToken<UserModel>() {}.type
+
+    private val sharedPref = context.getSharedPreferences(
+        context.getString(R.string.preference_file_key), Context.MODE_PRIVATE
+    )
+
+    override fun save(model: List<T>) {
+        val edit = sharedPref.edit()
+        model.forEach {
+            edit?.putString(it.getId().toString(), gson.toJson(it))
+        }
+        edit?.apply()
+    }
+
+    override fun fetch(id: Int): T? {
+        val allModels = fetchAll()
+        allModels?.forEach {
+            if (it.getId() == id) {
+                return it
+            }
+        }
+        return null
+    }
+
+    override fun fetchAll(): List<T>? {
+        val allModels: MutableList<T> = mutableListOf()
+
+        sharedPref.all?.values?.forEach {
+            allModels.add(gson.fromJson(it as String, type))
+        }
+        return allModels.toList()
+    }
+}
