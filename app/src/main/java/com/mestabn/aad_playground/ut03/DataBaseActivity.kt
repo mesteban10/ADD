c8cdd8034817a85518a0a70be61b0f60fa87c9fd
mesteban10 <mariaesteban1997@gmail.com>
2021-11-04 19:39:12 +0100

    Se crea nueva actividad

---
+++ app/src/main/java/com/mestabn/aad_playground/ut03/DataBaseActivity.kt
@@ -0,0 +1,12 @@
+package com.mestabn.aad_playground.ut03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mestabn.aad_playground.R
+
+class DataBaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_base)
+    }
+}
